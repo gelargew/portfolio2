5c7b75547c2e621aa177d9b3636fe4b79269b293
unknown <gelargew@gmail.com>
2022-06-26 20:31:44 +0700

    profile page animation

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -6,8 +6,24 @@ export default function Profile() {
     return (
         <MainLayout>
             <section className={styles.links}>
-                <a href='https://github.com/gelargew' target='_blank' rel='noreferrer' >GitHub ↗</a>
-                <a href='https://www.linkedin.com/in/gelar/' target='blank' rel='noreferrer'>LinkedIn ↗</a>
+                <a href='https://github.com/gelargew' target='_blank' rel='noreferrer' >
+                    <div className={styles.emailLink}>
+                            <p>GitHub ↗</p>
+                            <p>GitHub ↗</p>
+                    </div>
+                </a>
+                <a href='https://www.linkedin.com/in/gelar/' target='blank' rel='noreferrer'>
+                    <div className={styles.emailLink}>
+                            <p>LinkedIn ↗</p>
+                            <p>LinkedIn ↗</p>
+                    </div>
+                </a>
+                <a href='mailto:gelargew@gmail.com' target='_blank' rel='noreferrer' >
+                    <div className={styles.emailLink}>
+                        <p>Email ↗</p>
+                        <p>gelargew@gmail ↗</p>
+                    </div>
+                </a>
             </section>
             <section className={styles.certificate} >
                 <a href='https://certificates.cs50.io/89e51546-b427-4fd6-8400-e890c0a4ac54' target='_blank' rel='noreferrer'>Harvard cs50 2021 web programming certificate ↗</a>
