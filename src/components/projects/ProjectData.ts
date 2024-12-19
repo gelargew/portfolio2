9d61093001ab3ce6804072d8c2b0829a17eb8ea0
Gelar Rustiawan <gelargew@gmail.com>
2024-12-19 11:40:17 +0700

    q

--- src/components/projects/ProjectData.ts
+++ src/components/projects/ProjectData.ts
@@ -2,48 +2,81 @@ export const PROJECT_DATA = {
     frontend: [
         {
             title: 'pixtort',
-            description: 'interactive and customizable live image distortion effect using shaders.',
+            description:
+                'interactive and customizable live image distortion effect using shaders.',
             tech: ['glsl', 'threejs', 'r3f', 'vite'],
             date: 2022,
             imgURL: 'pixtort.webp',
             roles: ['developer'],
-            projectURL: 'https://distorted-pixel.vercel.app/'
+            projectURL: 'https://distorted-pixel.vercel.app/',
+        },
+        {
+            title: 'sein',
+            description: '3d Interactive web page',
+            tech: ['glsl', 'threejs', 'r3f', 'vite'],
+            date: 2022,
+            imgURL: 'sein.webp',
+            roles: ['developer'],
+            projectURL: 'https://sein.gelar.dev/',
+        },
+        {
+            title: 'atelier',
+            description: 'glsl/shaders experiments',
+            tech: ['glsl', 'threejs', 'r3f', 'vite'],
+            date: 2022,
+            imgURL: 'atelier.webp',
+            roles: ['developer'],
+            projectURL: 'https://atelier.gelar.dev/',
         },
         {
             title: 'yona yona weekenders',
-            description: 'an unofficial web page for japanese band yona yona weekenders.',
+            description:
+                'an unofficial web page for japanese band yona yona weekenders.',
             tech: ['sukuroru', 'gatsby', 'react', 'typescript'],
             date: 2022,
             imgURL: 'yonaweekenders.webp',
             roles: ['developer', 'designer'],
-            projectURL: 'https://yonaweekenders.web.app/'
+            projectURL: 'https://yonaweekenders.web.app/',
         },
         {
             title: 'faceting simulator',
-            description: 'a stone faceting simulator used and shared by the lost ark community.',
+            description:
+                'a stone faceting simulator used and shared by the lost ark community.',
             tech: ['vite', 'react', 'typescript'],
             date: 2022,
             imgURL: 'facet_sim.webp',
             roles: ['developer', 'designer'],
-            projectURL: 'https://stoneark.vercel.app/'
+            projectURL: 'https://stoneark.vercel.app/',
         },
+
         {
             title: 'covid 3d chart',
-            description: 'static web page that automatically being updated daily using github workflow and vercel',
+            description:
+                'static web page that automatically being updated daily using github workflow and vercel',
             tech: ['github workflow', 'threejs', 'react', 'vercel'],
             date: 2022,
             roles: ['developer'],
             imgURL: 'covid3d.webp',
-            projectURL: 'https://covid.gelar.dev/'
+            projectURL: 'https://covid.gelar.dev/',
+        },
+        {
+            title: 'Pluie - Weather Forecast',
+            description: 'Search a city and you might get some forecasting',
+            tech: ['vite', 'react', 'typescript'],
+            date: 2021,
+            imgURL: 'pluie.webp',
+            roles: ['developer', 'designer'],
+            projectURL: 'https://pluie.gelar.dev/',
         },
         {
             title: 'kanban board',
-            description: 'Interactive kanban board prototype with drag and drop function.',
+            description:
+                'Interactive kanban board prototype with drag and drop function.',
             tech: ['vite', 'react', 'typescript'],
             date: 2021,
             imgURL: 'kanban_board.webp',
             roles: ['developer'],
-            projectURL: 'https://kanban-board-gelar.vercel.app/'
+            projectURL: 'https://kanban-board-gelar.vercel.app/',
         },
         {
             title: 'ayykia',
@@ -52,16 +85,17 @@ export const PROJECT_DATA = {
             date: 2021,
             imgURL: 'ayykia.webp',
             roles: ['designer', 'developer'],
-            projectURL: 'https://ayykia.vercel.app/'
+            projectURL: 'https://ayykia.vercel.app/',
         },
         {
             title: 'スクロール / sukuroru',
-            description: 'a simple component to create a smooth scrolling experience for any react application',
+            description:
+                'a simple component to create a smooth scrolling experience for any react application',
             tech: ['react-spring', 'typescript'],
             date: 2021,
             roles: [],
-            projectURL: 'https://www.npmjs.com/package/sukuroru'
-        }
+            projectURL: 'https://www.npmjs.com/package/sukuroru',
+        },
     ],
     webgl: [
         {
@@ -71,34 +105,36 @@ export const PROJECT_DATA = {
             date: 2022,
             imgURL: 'genart.webp',
             roles: ['developer', 'designer'],
-            projectURL: 'https://art.gelar.dev/'
+            projectURL: 'https://art.gelar.dev/',
         },
         {
             title: 'shader gym',
-            description: 'collection of my first few shader experiment with glsl code included.',
+            description:
+                'collection of my first few shader experiment with glsl code included.',
             tech: ['glsl', 'threejs'],
             date: 2021,
             roles: ['developer'],
-            projectURL: 'https://gelargym-glsl.vercel.app/'
-        }
+            projectURL: 'https://gelargym-glsl.vercel.app/',
+        },
     ],
     fullstack: [
         {
             title: 'kpauli',
-            description: 'Kraepelin/pauli app, an online arithmetic test to view a person stress level. automatically give the result with graph, reducing the amount of time HR people test potential applicant.',
+            description:
+                'Kraepelin/pauli app, an online arithmetic test to view a person stress level. automatically give the result with graph, reducing the amount of time HR people test potential applicant.',
             tech: ['django', 'heroku', 'react', 'chartjs'],
             date: 2021,
             roles: ['developer'],
-            projectURL: 'https://github.com/gelargew/kraepelin-pauli'
+            projectURL: 'https://github.com/gelargew/kraepelin-pauli',
         },
         {
             title: 'construe',
-            description: 'final project of my harvard web programming class. a fullstack library management system.',
+            description:
+                'final project of my harvard web programming class. a fullstack library management system.',
             tech: ['django', 'react', 'drf'],
             date: 2021,
             roles: ['developer', 'designer'],
-            projectURL: 'https://github.com/gelargew/construe'
-        }
-
-    ]
+            projectURL: 'https://github.com/gelargew/construe',
+        },
+    ],
 }
