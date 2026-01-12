c8cf1ad02cd07a3b3344ee620b085e1765258e73
Gelar Rustiawan <gelar@plabs.id>
2026-01-12 14:17:30 +0700

    add gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,24 @@ import type { AppProps } from 'next/app'
 import TCanvas from '../components/three/TCanvas'
 import { Analytics } from '@vercel/analytics/react'
 import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            {/* Google tag (gtag.js) */}
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=AW-17869280502"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'AW-17869280502');
+                `}
+            </Script>
             <div className='App'>
                 <Head>
                     <title>Gelar Rustiawan</title>
